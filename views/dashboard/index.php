e256bf1ba1501e21be23bf17e50e750751b79629
iswanto <iswan1s71@gmail.com>
2024-10-24 23:11:25 +0700

    init dashboard

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -1,5 +1,30 @@
 <?php
 /** @var yii\web\View $this */
-?>
-<h1 class="text-secondary">Dashboard</h1>
 
+use yii\helpers\Url;
+
+?>
+<h4 class="text-secondary mb-4">Dashboard</h4>
+
+<div class="row mb-4">
+    <div class="col-xl-3 col-md-6">
+        <div class="card-box tilebox-two">
+            <a href="<?= Url::to(['ginee/index-serverside']) ?>" class="btn btn-sm btn-purple waves-effect waves-light float-right">View</a>
+            <h6 class="text-muted text-uppercase mt-0">Ginee</h6>
+            <h3 class="mb-4"><span>on progress</span></h3>
+            <div class="progress progress-md">
+                <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+        </div>
+    </div>  
+    <div class="col-xl-3 col-md-6">
+        <div class="card-box tilebox-two">
+            <a href="<?= Url::to(['shopee/index-serverside']) ?>" class="btn btn-sm btn-warning waves-effect waves-light float-right">View</a>
+            <h6 class="text-muted text-uppercase mt-0">Shopee</h6>
+            <h3 class="mb-4"><span>on progress</span></h3>
+            <div class="progress progress-md">
+                <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+        </div>
+    </div>
+</div>
