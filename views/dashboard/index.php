3ca90cb0a839a808522b8277bdd77140abee3f44
iswanto <iswan1s71@gmail.com>
2024-10-26 23:19:31 +0700

    update card dashboard

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -21,11 +21,21 @@ use yii\helpers\Url;
     */ ?>
     <div class="col-xl-3 col-md-6">
         <div class="card-box tilebox-two">
-            <a href="<?= Url::to(['shopee/index-serverside']) ?>" class="btn btn-sm btn-warning waves-effect waves-light float-right">View</a>
+            <a href="<?= Url::to(['shopee/index-summary']) ?>" class="btn btn-sm btn-warning waves-effect waves-light float-right">View</a>
             <h6 class="text-muted text-uppercase mt-0">Shopee</h6>
             <h3 class="mb-4"><span>on progress</span></h3>
             <div class="progress progress-md">
                 <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-md-6">
+        <div class="card-box tilebox-two">
+            <a href="<?= Url::to(['tiktok/index-summary']) ?>" class="btn btn-sm btn-dark waves-effect waves-light float-right">View</a>
+            <h6 class="text-muted text-uppercase mt-0">Tiktok</h6>
+            <h3 class="mb-4"><span>on progress</span></h3>
+            <div class="progress progress-md">
+                <div class="progress-bar progress-bar-striped bg-dark" role="progressbar" style="width: 95%" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
         </div>
     </div>
