0cb9d501d0c3cb9457bc49d17615516aba996fa9
iswanto <iswan1s71@gmail.com>
2025-03-09 21:35:14 +0700

    update utils

--- utils/StringHelper.php
+++ utils/StringHelper.php
@@ -102,4 +102,14 @@ class StringHelper
         return implode('', $words);
     }    
 
+    public static function intToMonth($month) {
+        $month = (int) $month;
+        $months = [
+            1 => 'Jan', 2 => 'Feb', 3 => 'Mar', 4 => 'Apr',
+            5 => 'May', 6 => 'Jun', 7 => 'Jul', 8 => 'Aug',
+            9 => 'Sep', 10 => 'Oct', 11 => 'Nov', 12 => 'Dec'
+        ];
+        return $months[$month];
+    }
+
 }
